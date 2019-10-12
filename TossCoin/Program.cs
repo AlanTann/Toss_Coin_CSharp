c8703cac8d2f7ec21c83c6dfe41d76f82eb9483e
Alan <tan_alan1020@hotmail.com>
2019-10-13 01:27:04 +0800

    Add Toss Coin Project

---
+++ TossCoin/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TossCoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hCount = 0;
+            int tCount = 0;
+            Console.WriteLine("1.Toss coin");
+            Console.WriteLine("2. Exit");
+            Console.Write("Enter choice: ");
+            int option = Convert.ToInt16(Console.ReadLine());
+
+            Boolean result;
+            while (option == 1)
+            {
+                result = CoinFlip();
+                if (result)
+                {
+                    hCount++;
+                }
+                else
+                {
+                    tCount++;
+                }
+                Console.WriteLine("Head: " + hCount + ", Tails: " + tCount + "\n");
+                Console.WriteLine("1.Toss coin");
+                Console.WriteLine("2. Exit");
+                Console.Write("Enter choice: ");
+                option = Convert.ToInt16(Console.ReadLine());
+            }
+        }
+
+        public static Boolean CoinFlip()
+        {
+            int coinFace;
+            Random rand = new Random();
+            coinFace = rand.Next(2);
+            if (coinFace == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }   
+    }
+}
+
